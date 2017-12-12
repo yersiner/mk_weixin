34c1dc10fca289bf04ac35dbfd02ab27e631aa0e
DESKTOP-BK9K98I\microtech <1282277054@qq.com>
2017-12-12 12:18:20 +0800

    增加安全域名

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ app.set('view engine', 'html');
 //设置静态目录
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'mp')));
-// 使用 LeanEngine 中间件
-// （如果没有加载云代码方法请使用此方法，否则会导致部署失败，详细请阅读 LeanEngine 文档。）
-// app.use(AV.Cloud);
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
